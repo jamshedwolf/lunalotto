4cc5605ca36a87d55d116af6be2146835b4e8e68
jamshedwolf <140429192+jamshedwolf@users.noreply.github.com>
2024-10-22 11:53:38 -0700

    videos update

--- src/Pages/Collection.jsx
+++ src/Pages/Collection.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import wave from "../assets/vave.svg";
-import card from "../assets/100.mov";
-import card2 from "../assets/500.mov";
-import card3 from "../assets/1000.mov";
-import card4 from "../assets/5000.mov";
-import card5 from "../assets/10000.mov";
+import card from "../assets/100.mp4";
+import card2 from "../assets/500.mp4";
+import card3 from "../assets/1000.mp4";
+import card4 from "../assets/5000.mp4";
+import card6 from "../assets/25000.mp4";
+import card7 from "../assets/50000.mp4";
+
+import card5 from "../assets/10000.mp4";
 import bitcoin from "../assets/bitcoin.svg";
 import eth from "../assets/eth.svg";
 
@@ -14,6 +17,8 @@ const collections = [
     { number: '1000', video: card3 },
     { number: '5000', video: card4 },
     { number: '10000', video: card5 },
+    { number: '25000', video: card6 },
+    { number: '500000', video: card7 },
 ];
 
 const stats = [
@@ -26,10 +31,10 @@ const stats = [
 export default function Collections() {
     return (
         <div id='collection' className="text-white  relative py-16 px-4 sm:px-6 lg:px-8">
-                <div className="absolute top-0 left-0 w-[100%] h-[100%] z-[-1] bgballs"></div>
+             
 
            
-              <div className="absolute top-0 left-0 w-[100%] h-[100%] z-[-5] bg5"></div>
+              <div className="absolute top-0 left-0 w-[100%] h-[100%] z-[-5] bg-[#052035]"></div>
             <div className="max-w-7xl flex flex-col items-center justify-center gap-10 mx-auto">
                 <div className="">
                     <h2 className="text-6xl jack lg:text-6xl text-center mb-4">
@@ -40,10 +45,10 @@ export default function Collections() {
                     </p>
                 </div>
                 {/* Collection Cards with Videos */}
-                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-4 mb-12">
+                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-7 gap-2  mb-12">
                     {collections.map((collection, index) => (
                         <div key={index} className="rounded-lg overflow-hidden">
-                            <div className="flex flex-col ">
+                            <div className="flex flex-col gap-2  ">
                                 <div className="flex justify-center items-center">
                                     {/* Video instead of image */}
                                     <video
@@ -52,11 +57,11 @@ export default function Collections() {
                                         loop
                                         muted
                                         playsInline
-                                        className="h-[500px]"
+                                        className="h-[300px]"
                                         alt={`Luna Collection ${collection.number}`}
                                     />
                                 </div>
-                                <button className="bg-[#85CD4F] mx-auto px-6 py-4 rounded-full max-w-fit font-semibold hover:bg-red-600 duration-300 delay-100 transition-colors">
+                                <button className="bg-[#85CD4F] mx-auto px-3 py-2 rounded-full max-w-fit font-semibold hover:bg-red-600 duration-300 delay-100 transition-colors">
                                     Mint Now
                                 </button>
                             </div>
